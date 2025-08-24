993c67b48d62c8c60b641bda95f439ec6ec56177
Sagar Kemble <sagarkemble.sasa@gmail.com>
2025-08-24 10:58:38 +0530

    configured server to run on vercel cloud functions

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 import express from "express";
 import cors from "cors";
 import admin from "firebase-admin";
-import "dotenv/config"; 
-
+import "dotenv/config";
 
 const app = express();
-app.use(cors({ origin: "*" }));app.use(express.json());
+app.use(cors({ origin: "*" }));
+app.use(express.json());
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
@@ -14,11 +14,14 @@ admin.initializeApp({
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   }),
 });
-
+// Subscribe to a topic
 app.post("/subscribe", async (req, res) => {
+  console.log("entered");
+
   const { token, topic } = req.body;
   try {
     const response = await admin.messaging().subscribeToTopic(token, topic);
+    console.log(response);
     console.log(`Successfully subscribed to topic: ${topic}`);
     res.json({ success: true, response });
   } catch (error) {
@@ -26,7 +29,6 @@ app.post("/subscribe", async (req, res) => {
     res.status(500).json({ success: false, error: error.message });
   }
 });
-
 // Send notification to a topic
 app.post("/send", async (req, res) => {
   const { topic, title, body } = req.body;
@@ -47,7 +49,17 @@ app.post("/send", async (req, res) => {
     res.status(500).json({ success: false, error: error.message });
   }
 });
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+// unsubscribe from a topic
+app.post("/unsubscribe", express.text({ type: "*/*" }), async (req, res) => {
+  const { token, topic } = JSON.parse(req.body);
+  try {
+    await admin.messaging().unsubscribeFromTopic(token, topic);
+    console.log(`Unsubscribed token from ${topic}`);
+    res.json({ success: true });
+  } catch (error) {
+    console.error("Unsubscribe error:", error);
+    res.status(500).json({ success: false, error: error.message });
+  }
 });
+
+export default app;
