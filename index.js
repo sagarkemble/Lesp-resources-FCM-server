309069a69d034569647c92a390e932424f2d18c7
Sagar Kemble <sagarkemble.sasa@gmail.com>
2025-08-30 15:55:40 +0530

    initial commit

--- index.js
+++ index.js
@@ -132,8 +132,8 @@ app.post("/unsubscribe", async (req, res) => {
   }
 });
 
-// app.listen(3000, () => {
-//   console.log("Server is running on port 3000");
-// });
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
 export default app;
